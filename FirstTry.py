06b336ab3f0a94ebb7ca65270f61d0d31ad48212
zpell057 <zpell057@uottawa.ca>
2024-09-28 22:23:40 -0400

    Added initial random way + cropping

--- FirstTry.py
+++ FirstTry.py
@@ -1,8 +1,10 @@
 import osmnx as ox
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
+import math
 def cutGraph(G,minX,maxX,minY,maxY):
-    nodesToKeep = [n for n, data in G.nodes(data=True) if (data['y'] > minY and data['y']<maxY and data['x']<maxX and data['x']>minX)]  # Example condition
+    nodesToKeep = [n for n, data in G.nodes(data=True) if (data['y'] > minY and data['y']<maxY and data['x']<maxX and data['x']>minX)]  
     out = G.subgraph(nodesToKeep)
     return(out)
 def kmToDeg(inp):
@@ -11,14 +13,16 @@ def cutGraphAround(G,posX,posY,dist):
     d=kmToDeg(dist)
     return(cutGraph(G,posX-d,posX+d,posY-d,posY+d))
 inputCoord = [-75.682316,45.421816]
-distanceWanted = 3
+distanceWanted = 10
+directionAngle = 2*math.pi*random.random()
+tempDirection = [inputCoord[0]+kmToDeg(distanceWanted)*math.cos(directionAngle)/1.8,inputCoord[1]+kmToDeg(distanceWanted)*math.sin(directionAngle)/1.8]
 G = ox.load_graphml(filepath='ottawaWithEnjoyabilityFixed.graphml')
 print("Loaded")
-G = cutGraph(G,inputCoord[0]-distanceWanted,inputCoord[0]+distanceWanted,inputCoord[1]-distanceWanted,inputCoord[1]+distanceWanted)
+G = cutGraphAround(G,inputCoord[0],inputCoord[1],distanceWanted*0.55)
 for u, v, data in G.edges(data=True):
             data['enjoyability'] = float(data['enjoyability'])
 orig = ox.distance.nearest_nodes(G,inputCoord[0],inputCoord[1])  
-dest = ox.distance.nearest_nodes(G,-75.679709,45.419708)  # CBY
+dest = ox.distance.nearest_nodes(G,tempDirection[0],tempDirection[1])  # CBY -75.679709,45.419708
 
 shortest_path = nx.shortest_path(G, orig, dest, weight='enjoyability')
 print("Shortest path found")
